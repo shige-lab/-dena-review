8d56f479f3423f25f699039c1eb517c50ff79c31
shige <shigebask@gmail.com>
2021-12-16 15:15:05 +0900

    test

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
+        <a
+          href="../public/index2.html"
+          >game</a>
+
       </header>
     </div>
   );
